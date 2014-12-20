9e1d9c640178c13661f86cd0756fa106bf0a3af7
varunsharma5 <varunge@gmail.com>
2014-12-20 20:18:30 +0530

    added code for bolts

--- src/main/java/storm/examples/WordCountTopology.java
+++ src/main/java/storm/examples/WordCountTopology.java
@@ -1,6 +1,10 @@
 package storm.examples;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
@@ -11,11 +15,12 @@ import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 
 /**
- * 
  * @author varun
- *
  */
+
 public class WordCountTopology {
+	private static Logger logger = LoggerFactory.getLogger(WordCountTopology.class);
+
 	public static class SplitSentence extends BaseRichBolt {
 		OutputCollector collector;
 
@@ -34,5 +39,36 @@ public class WordCountTopology {
 		public void declareOutputFields(OutputFieldsDeclarer declarer) {
 			declarer.declare(new Fields("word"));
 		}
+	}
+	
+	public static class WordCountBolt extends BaseRichBolt {
+		private Map<String, Integer> counts = null;
+		private OutputCollector collector;
+		public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+			counts = new HashMap<String, Integer>();
+			this.collector = collector;
+		}
+
+		public void declareOutputFields(OutputFieldsDeclarer declarer) {
+			declarer.declare(new Fields("word","count"));
+		}
+
+		public void execute(Tuple input) {
+
+			String word = input.getStringByField("word");
+			Integer count = counts.get(word);
+			
+			if(word == null) {
+				count = 0;
+			}
+			count++;
+			counts.put(word, count);
+			WordCountTopology.logger.info(word + "," + count);
+			collector.emit(new Values(word,count));
+		}
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
